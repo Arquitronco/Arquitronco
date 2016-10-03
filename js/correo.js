d91a582e16bb9dc9e6a517e7925a717a8ad1d9d8
Arturo Alvarado <a.jalvaradot@gmail.com>
2016-10-03 13:47:58 -0500

    working contact us

--- js/correo.js
+++ js/correo.js
@@ -7,5 +7,7 @@ function sendEmail() {
     $("input").val("");
     $("textarea").val("");
     alert("Gracias por contactarnos, le responderemos lo mas pronto posible.");
-     emailjs.send("default_service","arquitronco",{name:nameInput, phone:phoneInput,email:emailInput,comment:commentInput});
+    emailjs.send("default_service", "arquitronco", { name: nameInput, phone: phoneInput, email: emailInput, comment: commentInput });
+    emailjs.send("default_service", "arquitronco2", { name: nameInput, phone: phoneInput, email: emailInput, comment: commentInput });
+
 }
